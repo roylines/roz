2fa166e2c7f58ae6600d84eb127eced24074deb8
roylines <roy@roylines.co.uk>
2019-04-26 18:15:45 +0100

    opening the bastion

--- modules/lambda.tf
+++ modules/lambda.tf
@@ -1,3 +1,4 @@
+// create assumed role for lambda
 data "aws_iam_policy_document" "assume" {
   policy_id = "${var.name}-assume"
 
@@ -15,11 +16,13 @@ data "aws_iam_policy_document" "assume" {
   }
 }
 
+// create a role for the lambda to run under
 resource "aws_iam_role" "roz" {
   name               = "${var.name}"
   assume_role_policy = "${data.aws_iam_policy_document.assume.json}"
 }
 
+// create a policy document for iam permissions for the lambda
 data "aws_iam_policy_document" "roz" {
   policy_id = "${var.name}"
 
@@ -37,24 +40,37 @@ data "aws_iam_policy_document" "roz" {
       "arn:aws:logs:*:*:*",
     ]
   }
+  // to invoke self recursively
+  statement {
+    actions = ["lambda:InvokeFunction"]
+    effect  = "Allow"
+
+    resources = [
+      "arn:aws:lambda:*:*:function:${var.name}",
+    ]
+  }
 }
 
+// create the policy
 resource "aws_iam_policy" "roz" {
   name   = "${var.name}"
   policy = "${data.aws_iam_policy_document.roz.json}"
 }
 
+// attach the policy to the role
 resource "aws_iam_role_policy_attachment" "roz" {
   role       = "${aws_iam_role.roz.name}"
   policy_arn = "${aws_iam_policy.roz.arn}"
 }
 
+// archive the bundled lambda javascript file
 data "archive_file" "roz" {
   type        = "zip"
   source_file = "${path.module}/lambda.js"
   output_path = "${path.module}/lambda.zip"
 }
 
+// create the lambda
 resource "aws_lambda_function" "roz" {
   function_name                  = "${var.name}"
   filename                       = "${data.archive_file.roz.output_path}"
@@ -70,6 +86,7 @@ resource "aws_lambda_function" "roz" {
     variables = {
       TELEGRAM_TOKEN = "${var.telegram_token}"
       TELEGRAM_USER  = "${var.telegram_user}"
+      BASTION_AUTO_SCALING_GROUP = "${aws_autoscaling_group.bastion.name}"
     }
   }
 }
